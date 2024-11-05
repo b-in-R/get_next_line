48703d96eb7ba909f1961b6edb5f1eca24ffbf83
Raphaël <raphael.biner@gmail.com>
2024-11-05 18:20:24 +0100

    .

---
+++ get_next_line.h
@@ -0,0 +1,17 @@
+// HEADER
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <stdio.h> //
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+
+#endif
